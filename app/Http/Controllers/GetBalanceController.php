907106fe64cb410834944572e8ed8bbafd7fd71d
Helmi <muhamadkelmi@gmail.com>
2024-01-08 12:59:57 +0700

    1213

--- app/Http/Controllers/GetBalanceController.php
+++ app/Http/Controllers/GetBalanceController.php
@@ -72,6 +72,6 @@ class GetBalanceController extends Controller
 
     public function handleCallback(Request $request)
     {
-        return response()->json(['success' => true]);
+        return response()->json();
     }
 }
